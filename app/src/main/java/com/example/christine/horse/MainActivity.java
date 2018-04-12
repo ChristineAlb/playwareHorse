50b6734ed97a0d469f1f393f7a3d210435237ac5
sindribergsson <sindribergs@gmail.com>
2018-04-12 14:46:20 +0200

    Derp

--- app/src/main/java/com/example/christine/horse/MainActivity.java
+++ app/src/main/java/com/example/christine/horse/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements OnAntEventListene
     Boolean isConnected = false;
     Boolean pairing = false;
     Boolean updating = false;
-
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
