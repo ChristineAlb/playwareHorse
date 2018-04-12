9f048ef6bb9beae3cc2765528cd9342db2c73e4e
Christine <ca4294@gmail.com>
2018-04-12 14:42:55 +0200

    Game class updated to original :-)

--- app/src/main/java/com/example/christine/horse/MainActivity.java
+++ app/src/main/java/com/example/christine/horse/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.christine.horse;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.livelife.motolibrary.Game;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,6 +12,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
 }
