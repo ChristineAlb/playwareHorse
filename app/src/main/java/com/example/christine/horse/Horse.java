1e8af47f324a25d2195bfcc6c8b8e00204bc9e55
Jannik Gerdt <jannik_gerdt@hotmail.dk>
2018-04-22 14:07:56 +0200

    layout

--- app/src/main/java/com/example/christine/horse/Horse.java
+++ app/src/main/java/com/example/christine/horse/Horse.java
@@ -92,48 +92,5 @@ public class Horse extends Game {
         };
         return lightDelay;
     }
-/*
-    // Initialize Horse variables
-    private int player = 1;
-    private int move = 1;
-    private int totalMoves = 4;
-    private int round = 1;
-    private int numPlayers = getNumPlayers();
-
-    public void addToPattern(ArrayList sequence, int numberOfNew) {
-        for (int i = 0; i < numberOfNew; i++) {
-            sequence.append(input[i]);
-        }
-    }
-
-    public boolean comparePattern(ArrayList sequence) {
-        for (int i = 0; i < sequence.length; i++) {
-            if (input[i] != sequence[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public void play() {
-        ArrayList sequence = <>;
-        addToPattern(sequence, 4);
-        this.round++;
-        this.player++;
-        boolean playing = comparePattern(sequence);
-
-        while (playing) {
-            addToPattern(sequence, 1);
-            this.round++;
-            this.player++;
-            if (this.player > this.numPlayers) {
-                this.player = 1;
-            }
-            playing = comparePattern(sequence);
-        }
-
-        // When while loop stops, someone made a wrong step in the sequence
-
-    }*/
 }
 
