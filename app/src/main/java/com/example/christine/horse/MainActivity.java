e85827573c2270b3591ec8e3e465f88346a5081c
Jannik Gerdt <jannik_gerdt@hotmail.dk>
2018-04-26 15:43:23 +0200

    update

--- app/src/main/java/com/example/christine/horse/MainActivity.java
+++ app/src/main/java/com/example/christine/horse/MainActivity.java
@@ -213,7 +213,7 @@ public class MainActivity extends AppCompatActivity implements OnAntEventListene
                 MainActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        
+
                     }
                 });
             }
